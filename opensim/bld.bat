122fcfaada481144dbde88e478c20ad8a09f0ea1
Lukasz Kidzinski <Lukasz Kidzinski>
2017-01-14 19:14:27 -0800

    Some fixes for windows

--- opensim/bld.bat
+++ opensim/bld.bat
@@ -1,18 +1,10 @@
 mkdir opensim_dependencies_build
 cd .\opensim_dependencies_build
-cmake ..\opensim-core\dependencies
-  -G"Visual Studio 14 2015 Win64"
-  -DCMAKE_INSTALL_PREFIX="$PREFIX"
-cmake --build . --config RelWithDebInfo -- /maxcpucount:8
+cmake ..\dependencies -G"Visual Studio 14 2015 Win64" -DCMAKE_INSTALL_PREFIX="%PREFIX%"
+cmake --build . --config RelWithDebInfo --target install -- /maxcpucount:8
 cd ..
 
 mkdir opensim_build
 cd .\opensim_build
-cmake ..\opensim-core
-  -G"Visual Studio 14 2015 Win64"
-  -DCMAKE_INSTALL_PREFIX="$PREFIX"
-  -DOPENSIM_DEPENDENCIES_DIR="$PREFIX"
-  -DBUILD_JAVA_WRAPPING=ON
-  -DBUILD_PYTHON_WRAPPING=ON
-  -DWITH_BTK=ON
-cmake --build . --config RelWithDebInfo -- /maxcpucount:8
+cmake ..\ -G"Visual Studio 14 2015 Win64" -DCMAKE_INSTALL_PREFIX="%PREFIX%" -DOPENSIM_DEPENDENCIES_DIR="%PREFIX%" -DBUILD_PYTHON_WRAPPING=ON -DWITH_BTK=ON
+cmake --build . --config RelWithDebInfo --target install -- /maxcpucount:8
