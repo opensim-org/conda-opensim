c107a940830a806b4e2418ea8f6fb7127f0f098c
Lukasz Kidzinski <lukasz.kidzinski@gmail.com>
2017-01-16 12:51:20 -0800

    Switched to Release (300mb -> 30mb) and added openblas to dependencies

--- opensim/build.sh
+++ opensim/build.sh
@@ -7,7 +7,7 @@ mkdir opensim_dependencies_build
 cd opensim_dependencies_build
 cmake ../dependencies/ \
       -DCMAKE_INSTALL_PREFIX="$PREFIX" \
-      -DCMAKE_BUILD_TYPE=RelWithDebInfo
+      -DCMAKE_BUILD_TYPE=Release
 make -j8
 cd ..
 
@@ -16,7 +16,7 @@ mkdir opensim_build
 cd opensim_build
 cmake ../ \
       -DCMAKE_INSTALL_PREFIX="$PREFIX" \
-      -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+      -DCMAKE_BUILD_TYPE=Release \
       -DOPENSIM_DEPENDENCIES_DIR="$PREFIX" \
       -DBUILD_PYTHON_WRAPPING=ON \
       -DBUILD_JAVA_WRAPPING=OFF \
