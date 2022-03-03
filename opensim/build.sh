712e67ec080c2009856fa2b32d477fd148d03c79
aymanhab <ahabib@stanford.edu>
2022-03-03 12:13:45 -0800

    Working conda build on linux without moco

--- opensim/build.sh
+++ opensim/build.sh
@@ -4,35 +4,43 @@ mkdir opensim_dependencies_build
 cd opensim_dependencies_build
 cmake ../dependencies/ -LAH \
       -DCMAKE_INSTALL_PREFIX="$PREFIX" \
+      -DCMAKE_INSTALL_LIBDIR=lib \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_OSX_SYSROOT=${CONDA_BUILD_SYSROOT} \
       -DCMAKE_OSX_DEPLOYMENT_TARGET=10.15 \
-      -DSUPERBUILD_ezc3d=ON \
-      -DSUPERBUILD_adolc=OFF \
       -DSUPERBUILD_casadi=OFF \
+      -DSUPERBUILD_adolc=OFF \
       -DSUPERBUILD_colpack=OFF \
-      -DSUPERBUILD_ipopt=OFF
+      -DSUPERBUILD_docopt=ON \
+      -DSUPERBUILD_eigen=OFF \
+      -DSUPERBUILD_ipopt=OFF \
+      -DSUPERBUILD_simbody=ON \
+      -DSUPERBUILD_spdlog=ON \
+      -DSUPERBUILD_ezc3d=ON 
 make -j8
 cd ..
 
-cp -r $PREFIX/simbody/libexec/simbody/* $PREFIX/bin/
+#cp -r $PREFIX/simbody/libexec/simbody/* $PREFIX/bin/
 
 # TODO: Tests are missing!
 mkdir opensim_build
 cd opensim_build
 cmake ../ \
       -DCMAKE_INSTALL_PREFIX="$PREFIX" \
+      -DCMAKE_INSTALL_LIBDIR=lib \
       -DCMAKE_BUILD_TYPE=Release \
       -DOPENSIM_DEPENDENCIES_DIR="$PREFIX" \
       -DCMAKE_OSX_SYSROOT=${CONDA_BUILD_SYSROOT} \
       -DCMAKE_OSX_DEPLOYMENT_TARGET=10.15 \
       -DBUILD_PYTHON_WRAPPING=ON \
       -DOPENSIM_DISABLE_LOG_FILE=ON \
-      -DOPENSIM_PYTHON_CONDA=ON \
       -DOPENSIM_C3D_PARSER=ezc3d \
       -DBUILD_TESTING=OFF \
-      -DTROPTER_WITH_SNOPT=OFF \
-      -DTROPTER_WITH_OPENMP=OFF \
+      -DOPENSIM_PYTHON_STANDALONE=ON \
+      -DOPENSIM_INSTALL_UNIX_FHS=on \
+      -DBUILD_API_ONLY=ON \
+      -DOPENSIM_BUILD_INDIVIDUAL_APPS_DEFAULT=OFF \
+      -DOPENSIM_COPY_DEPENDENCIES=ON \
       -DOPENSIM_WITH_TROPTER=OFF \
       -DOPENSIM_WITH_CASADI=OFF
 make -j8
